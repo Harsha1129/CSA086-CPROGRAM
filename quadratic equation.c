29a7281580c7b9514a65d1f0d08d49ac7ed2a083
Harsha1129 <124118292+Harsha1129@users.noreply.github.com>
2023-03-08 13:26:21 +0530

    Create quadratic equation.c

---
+++ quadratic equation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+ void main()
+{
+ int a,b,c,root1,root2;
+ int d;
+ Printf("Enter the value a \n");
+ Scanf("%d",&a);
+ Printf("Enter the value b \n");
+ Scanf("%d",&b);
+ Printf("Enter the value c \n");
+ Scanf("%d",&c);
+ d=((b*b)-(4*a*c));
+if(d>0)
+ {
+ root1=(-b+Sqrt((b*b)-(4*a*c))/(2*a);
+ root2=(-b-Sqrt((b*b)-(4*a*c))/(2*a);
+ Printf("\n first root is %d",root1);
+ Printf("\n second root is %d",root2);
+ Printf("\n the root are real&unequal");
+ }
+elseif(d==0)
+ {
+  root1=(-b+Sqrt((b*b)-(4*a*c))/(2*a);
+  root2=(-b-Sqrt((b*b)-(4*a*c))/(2*a);
+ Printf("\n first root is %d",root
