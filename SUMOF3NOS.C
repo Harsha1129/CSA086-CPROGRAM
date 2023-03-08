88085bcee7cdbc4b91962a3d8e004eb492621aed
Harsha1129 <124118292+Harsha1129@users.noreply.github.com>
2023-03-08 09:04:48 +0530

    Create SUMOF3NOS.C

---
+++ SUMOF3NOS.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{
+float a,b,c;
+scanf("%f",&a);
+scanf("%f",&b);
+scanf("%f",&c);
+if(a>b)
+   printf("a is big");
+else
+   printf("b is big");
+if(b>c)
+   printf("c is big");
+}
